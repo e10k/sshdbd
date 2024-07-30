2fe588fb1e174ca48d92210dca694f4dbf3ac890
e10k <e@e10k.dev>
2024-07-30 22:12:20 +0300

    ssh server

--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module github.com/e10k/dbdl
 go 1.21.1
 
 require (
+	github.com/BurntSushi/toml v1.4.0
 	github.com/gliderlabs/ssh v0.3.7
-	golang.org/x/crypto v0.25.0
 )
 
 require (
-	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/BurntSushi/toml"
 	"github.com/e10k/dbdl/config"
 	"github.com/e10k/dbdl/mysqldump"
+	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
 )
 
 func main() {
+	known, comment, _, _, err := gossh.ParseAuthorizedKey([]byte("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMgfBf9NEfplAqXjMdiHCPM0J+f6JVJX4BE2SfEkvPr emi"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	connId := "main"
 	dbName := "sakila"
 
 	var conf config.Config
-	_, err := toml.DecodeFile("config.toml", &conf)
+	_, err = toml.DecodeFile("config.toml", &conf)
 	if err != nil {
 		panic(err)
 	}
@@ -29,8 +37,20 @@ func main() {
 
 	conn.Dbname = dbName
 
-	err = mysqldump.Dump(&conn, os.Stdout, os.Stderr)
-	if err != nil {
-		panic(err)
-	}
+	ssh.Handle(func(s ssh.Session) {
+		//s.Stderr().Write([]byte(comment))
+		log.Println(comment)
+		err = mysqldump.Dump(&conn, s, s.Stderr())
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	publicKeyOption := ssh.PublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
+		return ssh.KeysEqual(key, known)
+		// return true // allow all keys, or use ssh.KeysEqual() to compare against known keys
+	})
+
+	log.Println("starting ssh server on port 2222...")
+	log.Fatal(ssh.ListenAndServe(":2222", nil, publicKeyOption))
 }
