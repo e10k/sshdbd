10d4061f83fb50aa394373aefaea919c20593994
e10k <e@e10k.dev>
2024-08-07 19:41:56 +0300

    more refactoring

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/e10k/dbdl/config"
 	"github.com/e10k/dbdl/db"
+	"github.com/e10k/dbdl/input"
 	"github.com/gliderlabs/ssh"
 	gossh "golang.org/x/crypto/ssh"
 )
@@ -25,7 +26,7 @@ func main() {
 	fmt.Fprintf(os.Stderr, "conf: %v\n", conf)
 
 	sessionHandler := func(s ssh.Session) {
-		connId, dbName, skippedTables, err := parseInput(s.User())
+		connId, dbName, skippedTables, err := input.ParseInput(s.User())
 		if err != nil {
 			panic(err)
 		}
@@ -81,30 +82,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func parseInput(s string) (string, string, []string, error) {
-	s = strings.Trim(s, " ")
-	slice := strings.Split(s, ":")
-
-	// a valid input will have one of these forms:
-	//    main:sakila (connection id + database name)
-	//    main:sakila:table_1,table_2,table_3 (connection id + database name + comma separated list of table names)
-	l := len(slice)
-	if l == 3 {
-		var tables []string
-		for _, t := range strings.Split(slice[2], ",") {
-			t = strings.Trim(t, " ")
-			if len(t) > 0 {
-				tables = append(tables, t)
-			}
-		}
-		return slice[0], slice[1], tables, nil
-	} else if l == 2 {
-		return slice[0], slice[1], nil, nil
-	}
-
-	return "", "", nil, fmt.Errorf("unexpected input data format: %s", s)
 }
 
 func getKeys() []string {
