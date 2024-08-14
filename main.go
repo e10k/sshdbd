97a84d33bc778f79a1c3916734e43b57f366846c
e10k <e@e10k.dev>
2024-08-14 21:34:38 +0300

    implement install command

--- main.go
+++ main.go
@@ -15,9 +15,6 @@ import (
 )
 
 func main() {
-	// hk, err := server.GenerateHostKeyBytes()
-	// os.WriteFile("test.pem", hk, 0600)
-
 	flag.Parse()
 	if len(flag.Args()) < 1 {
 		fmt.Println("Please specify a command.")
@@ -35,6 +32,50 @@ func main() {
 		fmt.Printf("Unknown command: %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func handleInstallCommand(args []string) {
+	_ = args
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("error obtaining the home dir: %s", err)
+	}
+
+	configDir := home + "/.sshdbd"
+	err = os.MkdirAll(configDir, 0700)
+	if err != nil {
+		log.Fatalf("error creating the dir %s", err)
+	}
+
+	authorizedKeysFile := configDir + "/authorized_keys"
+	f, err := os.Create(authorizedKeysFile)
+	if err != nil {
+		log.Fatalf("error creating %s: %s", authorizedKeysFile, err)
+	}
+
+	err = f.Chmod(0600)
+	if err != nil {
+		log.Fatalf("error setting permissions for %s: %s", authorizedKeysFile, err)
+	}
+
+	hk, err := server.GenerateHostKeyBytes()
+	hostKeyFile := configDir + "/hostkey.pem"
+	err = os.WriteFile(hostKeyFile, hk, 0600)
+	if err != nil {
+		log.Fatalf("error creating %s: %s", hostKeyFile, err)
+	}
+
+	configFile := configDir + "/config.toml"
+	f, err = os.Create(configFile)
+	if err != nil {
+		log.Fatalf("error creating %s: %s", configFile, err)
+	}
+	f.WriteString(fmt.Sprintf("[connections.main]\nhost = %q\nport = %d\nusername = %q\npassword = %q\n\n", "localhost", 3306, "usr", "pass"))
+}
+
+func handleServeCommand(args []string) {
+	_ = args
 
 	var conf config.Config
 	_, err := toml.DecodeFile("config.toml", &conf)
@@ -47,7 +88,7 @@ func main() {
 
 	srv := server.NewServer(conf)
 
-	privateBytes, err := os.ReadFile("id_rsa")
+	privateBytes, err := os.ReadFile("hostkey.pem")
 	if err != nil {
 		log.Fatal("failed to load private key: ", err)
 	}
@@ -63,12 +104,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func handleInstallCommand(args []string) {
-	//
-}
-
-func handleServeCommand(args []string) {
-	//
 }
